9540fbbb8995805a18f55e0a0f9af45293783abc
gilsaia <794433219@qq.com>
2021-09-06 23:34:54 +0800

    update:tomato clock change

--- personal-plan-assistant/components/ppaTomatoClock.tsx
+++ personal-plan-assistant/components/ppaTomatoClock.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import { Button, Progress, Space, Statistic } from 'antd'
+import {
+  Button,
+  InputNumber,
+  Popover,
+  Progress,
+  Space,
+  Statistic,
+  Typography
+} from 'antd'
 import moment, { Duration, Moment } from 'moment'
+import { PpaIconButton } from './ppaIconButton'
+import { CheckCircleOutlined } from '@ant-design/icons'
 const { Countdown } = Statistic
 
 interface tomatoClockState {
   countdownValue: number
-  countString: string
   countValue: Duration
   beginCount: boolean
-  countPercent:number
+  countPercent: number
+  popVisible: boolean
+  popValue: number
 }
 
 export class PpaTomatoClock extends React.Component<any, tomatoClockState> {
@@ -16,21 +27,26 @@ export class PpaTomatoClock extends React.Component<any, tomatoClockState> {
     super(props)
     this.state = {
       countdownValue: moment().valueOf(),
-      countString: '25分钟',
       countValue: moment.duration(25, 'm'),
       beginCount: false,
-      countPercent:100
+      countPercent: 100,
+      popVisible: false,
+      popValue: 25
     }
     this.progressText = this.progressText.bind(this)
     this.handleStartCount = this.handleStartCount.bind(this)
     this.handleEndCount = this.handleEndCount.bind(this)
-    this.onCountChange=this.onCountChange.bind(this)
+    this.handlePopVisible = this.handlePopVisible.bind(this)
+    this.onCountChange = this.onCountChange.bind(this)
     this.onCountFinish = this.onCountFinish.bind(this)
+    this.onClockValueChange = this.onClockValueChange.bind(this)
+    this.onClockChange = this.onClockChange.bind(this)
   }
 
-  onCountChange(value?:number|string):void {
+  onCountChange(value?: number | string): void {
     this.setState({
-      countPercent:((value as number)/this.state.countValue.asMilliseconds())*100
+      countPercent:
+        ((value as number) / this.state.countValue.asMilliseconds()) * 100
     })
   }
 
@@ -38,17 +54,28 @@ export class PpaTomatoClock extends React.Component<any, tomatoClockState> {
     this.setState({ beginCount: false })
   }
 
+  onClockValueChange(value: number) {
+    value = Math.floor(value)
+    this.setState({ popValue: value })
+  }
+
+  onClockChange() {
+    this.setState({
+      countValue: moment.duration(this.state.popValue, 'm'),
+      popVisible: false
+    })
+  }
+
   progressText(percent?: number): React.ReactNode {
     return (
       <Countdown
-        title={this.state.countString}
+        title={this.state.countValue.humanize()}
         value={this.state.countdownValue}
         onFinish={this.onCountFinish}
         onChange={this.onCountChange}
       />
     )
   }
-
 
   handleStartCount() {
     this.setState({
@@ -62,6 +89,10 @@ export class PpaTomatoClock extends React.Component<any, tomatoClockState> {
       countdownValue: moment().valueOf(),
       beginCount: false
     })
+  }
+
+  handlePopVisible(visible: boolean) {
+    this.setState({ popVisible: visible })
   }
 
   render() {
@@ -79,6 +110,22 @@ export class PpaTomatoClock extends React.Component<any, tomatoClockState> {
         </Button>
       )
     }
+    const popContent = (
+      <>
+        <Space>
+          <InputNumber
+            min={1}
+            max={240}
+            defaultValue={25}
+            onChange={this.onClockValueChange}
+          />
+          <Typography.Text>分钟</Typography.Text>
+          <Button type={'primary'} onClick={this.onClockChange}>
+            提交
+          </Button>
+        </Space>
+      </>
+    )
     return (
       <Space direction={'vertical'} align={'center'}>
         <Progress
@@ -89,7 +136,15 @@ export class PpaTomatoClock extends React.Component<any, tomatoClockState> {
           strokeColor={'orange'}
         />
         <Space>
-          <Button>修改时间</Button>
+          <Popover
+            content={popContent}
+            title={'番茄钟时间'}
+            trigger={'click'}
+            visible={this.state.popVisible}
+            onVisibleChange={this.handlePopVisible}
+          >
+            <Button>修改时间</Button>
+          </Popover>
           {countButton}
         </Space>
       </Space>
